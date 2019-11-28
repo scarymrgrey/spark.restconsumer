071ba9dcc146b3334ceb193c0d3a6039395f8f1c
Scary MrGrey <imbamrgrey@gmail.com>
2019-11-29 00:37:14 +0100

    concurrent Http requests

--- src/main/scala/CurrencyResponseIterator.scala
+++ src/main/scala/CurrencyResponseIterator.scala
@@ -1,7 +1,9 @@
 import net.liftweb.json.Serialization.write
 import net.liftweb.json.{DefaultFormats, compactRender, parse}
 import scalaj.http.Http
-import scala.collection.immutable
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 class CurrencyResponseIterator(inner: Iterator[CurrencyRequest], url: String, batchSize: Int) extends Iterator[String] {
 
@@ -9,16 +11,31 @@ class CurrencyResponseIterator(inner: Iterator[CurrencyRequest], url: String, ba
 
   private var buffer: List[String] = List()
 
-  private def getNewBatch: immutable.List[String] = {
+  private def getNewBatch: List[String] = {
+    implicit val ec = ExecutionContext.global
     implicit val formats: DefaultFormats.type = DefaultFormats
-    val batch = inner.take(batchSize).toArray
-    val json: String = write(batch)
-    val response = Http(s"$url/currency")
-      .postData(json)
-      .header("content-type", "application/json")
-      .asString
-    val resp = parse(response.body)
-    resp.children.map(compactRender)
+
+    def lift[T](futures: Seq[Future[T]]): Seq[Future[Try[T]]] =
+      futures.map(_.map {
+        Success(_)
+      }.recover { case t => Failure(t) })
+
+    val tasks = inner.take(batchSize).map { z =>
+      Future {
+        val json: String = write(Array(z))
+        val response = Http(s"$url/currency")
+          .postData(json)
+          .header("content-type", "application/json")
+          .asString
+        compactRender(parse(response.body))
+      }
+    }.toSeq
+
+    val succeededTasks = Await.result(Future.sequence(lift(tasks)), Duration.Inf)
+      .filterNot(_.isFailure)
+      .map(z => z.get)
+
+    succeededTasks.toList
   }
 
   override def next(): String = {
